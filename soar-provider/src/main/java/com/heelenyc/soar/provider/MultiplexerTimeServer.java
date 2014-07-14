9e6810516850d106e30ef4c34c55ce1120a26590
heelenyc <heelenyc@gmail.com>
2014-07-14 16:45:07 +0800

    netty time server

---
+++ soar-provider/src/main/java/com/heelenyc/soar/provider/MultiplexerTimeServer.java
@@ -0,0 +1,137 @@
+package com.heelenyc.soar.provider;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * @author yicheng
+ * @since 2014年6月28日
+ * 
+ */
+public class MultiplexerTimeServer implements Runnable {
+
+	private Selector selector;
+	private ServerSocketChannel serverSocketChannel;
+
+	private volatile boolean stop;
+
+	public MultiplexerTimeServer(int port) {
+		try {
+			selector = Selector.open();
+			serverSocketChannel = ServerSocketChannel.open();
+
+			serverSocketChannel.configureBlocking(false);
+			serverSocketChannel.socket().bind(new InetSocketAddress(port),4);
+			serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+			System.out.println("The time server start on port " + port);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+
+	public void stop() {
+		this.stop = true;
+	}
+
+	@Override
+	public void run() {
+		while (!stop) {
+			try {
+				selector.select(1000);
+				Set<SelectionKey> selectedKeys = selector.selectedKeys();
+				Iterator<SelectionKey> it = selectedKeys.iterator();
+				SelectionKey key = null;
+				System.out.println("selected keyset size " + selectedKeys.size());
+				while (it.hasNext()) {
+					key = it.next();
+					try {
+						handleInput(key);
+					} catch (Exception e) {
+						if (key != null) {
+							key.cancel();
+							if (key.channel() != null) {
+								key.channel().close();
+							}
+						}
+					}
+					it.remove();
+				}
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		if (selector != null) {
+			try {
+				selector.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private void handleInput(SelectionKey key) throws IOException {
+		if (key.isValid()) {
+			if (key.isAcceptable()) {
+				ServerSocketChannel ssc = (ServerSocketChannel) key.channel();
+				SocketChannel sc = ssc.accept();
+				sc.configureBlocking(false);
+				sc.register(selector, SelectionKey.OP_READ);
+				System.out.println("accept and register read op");
+			}
+			if (key.isReadable()) {
+				SocketChannel sc = (SocketChannel) key.channel();
+				ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+				int readBytes = sc.read(readBuffer);
+				if (readBytes > 0) {
+					readBuffer.flip();
+					byte[] bytes = new byte[readBuffer.remaining()];
+					readBuffer.get(bytes);
+					String body = new String(bytes, "UTF-8");
+					System.out
+							.println("The time server recieved order " + body);
+					String currentTime = "QUERY TIME ORDER"
+							.equalsIgnoreCase(body) ? new Date().toString()
+							: "BAD ORDER";
+
+					doWrite(sc,currentTime);
+					System.out.println("receive and send response");
+				}else if (readBytes < 0) {
+					key.cancel();
+					sc.close();
+					System.out.println("receive byte -1 , close channle");
+				}else {
+					
+				}
+			}
+		}
+	}
+
+	/**
+	 * @param sc
+	 * @param currentTime
+	 * @throws IOException 
+	 */
+	private void doWrite(SocketChannel sc, String response) throws IOException {
+		if (response != null && response.length() > 0) {
+			byte[] bytes = response.getBytes();
+			ByteBuffer writeBuffer = ByteBuffer.allocate(bytes.length);
+			writeBuffer.put(bytes);
+			writeBuffer.flip();
+			sc.write(writeBuffer);
+		}
+	}
+
+}
---
+++ soar-provider/src/main/java/com/heelenyc/soar/provider/TimeServer.java
@@ -0,0 +1,17 @@
+package com.heelenyc.soar.provider;
+
+
+/**
+ * @author yicheng
+ * @since 2014年6月28日
+ *
+ */
+public class TimeServer {
+	public static void main(String args[]){
+		int port = 8080;
+		
+		MultiplexerTimeServer server = new MultiplexerTimeServer(port);
+		
+		new Thread(server,"NIO-MultiplexerTimeServer-001").start();
+	}
+}
